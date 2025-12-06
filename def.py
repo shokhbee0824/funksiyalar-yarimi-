209bb703dbdd0522b2f5de6d4c48ddc76a5260c4
anonim <baxtiyarovshohjaxon0@gmail.com>
2025-12-06 16:28:55 +0500

    Create def.py

---
+++ def.py
@@ -0,0 +1,82 @@
+# 1-masala
+# def kvadrat(sonlar):
+#     son=sonlar**2
+#     return son
+# n = int(input("son kiriting = "))
+# print(kvadrat(n))
+# 2-masala
+# def takrorlash(matn,n):
+#     return matn *n
+# matn = input("matn kiriting  = ")
+# n = int(input ("necha marta takrorlansin = "))
+# print(takrorlash(matn,n))
+# 3-masala
+# def juf_toq(son):
+#     if son % 2 == 0 :
+#         return "juft"
+#     else:
+#         return "toq"
+# son = int(input("sonni kiriting  = "))
+# print(f"son {son} , bu son ---> {juf_toq(son)}")
+# 4-masala
+# def yigindi (a,b):
+#     n = a+b
+#     return n
+# a = int(input("a ni kiriting = "))
+# b = int(input("b ni kiriting = "))
+# print(yigindi(a,b))
+# 5-masala
+# def harf(matn):
+#     hisob= 0
+#     for harf in matn :
+#         hisob += 1
+#     return f"uzunlik {hisob}"
+# matn = input("matn kieiting = ")
+# print(harf(matn))
+# 6-maslaa
+# def mus_manf(son):
+#     if son < 0 :
+#         return "manfiy "
+#     elif son == 0 :
+#         return "neytral"
+#     else:
+#         return "musbat"
+# son = int(input("son kiriting  = "))
+# print(mus_manf(son))
+# 7-masala
+# def modul(son):
+#     n = abs(son)
+#     return n
+# son =int(input("son kiriting modulda chiqadi : "))
+# print(modul(son))
+# 8-masala
+# def teskari (matn):
+#     n = matn[::-1]
+#     return n
+# matn = input("matn kiriting = ")
+# print(teskari(matn))
+# 2- modl\
+# 1-m
+# def foktoriol(m):
+#     if m == 0 or m == 1  :
+#         return 1
+#     else:
+#         return m * foktoriol(m-1)
+# print(foktoriol(5))
+# 2-m
+# def yigindi (son):
+#     yigindi = 0
+#     for son in son:
+#         if son %2 == 0 :
+#             yigindi += son
+#     return yigindi
+# royxat = [1,2,4,6,8]
+# print(yigindi(royxat))
+# 3-m
+# 4-m
+# def daraja (son):
+#     n = son **(son1)
+#     return n
+# son =int(input("sonni kiriting = "))
+# son1 =int(input("nechanchi daraja ? : "))
+# print(daraja (son))
